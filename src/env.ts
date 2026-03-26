4cfd0e61e29eb165286a2f1ef7996c63ba6a5e1c
Josh-Estrada <josh.est456@gmail.com>
2026-03-26 08:22:39 -0700

    Chore: modify path in index, modify app title, fix spacing in env.ts

--- src/env.ts
+++ src/env.ts
@@ -1,3 +1,2 @@
 export const BASE_URL = import.meta.env.BASE_URL
-export const VITE_GENESYS_OAUTH_CLIENT_ID = import.meta.env
-  .VITE_GENESYS_OAUTH_CLIENT_ID
+export const VITE_GENESYS_OAUTH_CLIENT_ID = import.meta.env.VITE_GENESYS_OAUTH_CLIENT_ID
